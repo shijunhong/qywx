caad3c2bc405cd19b69aaf97fb1129f804ac41f3
verybigorange <zhoucheng@rsung.com>
2018-08-17 18:14:39 +0800

    增加bottom组件

--- src/views/invite/index.vue
+++ src/views/invite/index.vue
@@ -1,20 +1,25 @@
 <template>
-  <tap tap1="添加到我的客户" tap2="关注微工作台">
-      <add slot="tap1"/>
-      <attention slot="tap2"/>
-  </tap>
+  <div>
+      <tap tap1="添加到我的客户" tap2="关注微工作台">
+        <add slot="tap1"/>
+        <attention slot="tap2"/>
+    </tap>
+    <bottom />
+  </div>
 </template>
 
 <script>
 import Tap from 'components/Tap'
 import add from './add'
 import attention from './attention'
+import Bottom from 'components/Bottom'
 
 export default {
   components: {
     Tap,
     add,
-    attention
+    attention,
+    Bottom
   }
 }
 </script>
