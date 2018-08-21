70cff6e968828a65fb388af8f543f6ede4aa09b6
verybigorange <zhoucheng@rsung.com>
2018-08-21 17:20:46 +0800

    修改企业微信的回调地址

--- src/config.js
+++ src/config.js
@@ -6,3 +6,6 @@ export const host = 'http://crmapif1.newdhb.com'
 
 // 正式环境
 // export const host = ''
+
+// 测试企业微信的回调地址
+export const redirectHost = 'http://crmf1.newdhb.com'
--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,12 +3,12 @@
 </template>
 
 <script>
-import { host } from '@/config'
+import { redirectHost } from '@/config'
 
 export default {
   mounted() {
     if (!this.$route.query.code) {
-      const url = `${host}/login`
+      const url = `${redirectHost}/login`
       window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wwf8a477207b560622&redirect_uri=${url}&response_type=code&scope=SCOPE&agentid=AGENTID&state=STATE#wechat_redirect`
       return
     }
